9fef7cb8d7a137fef1e18f7b1ae78dbf62f35ed9
Eduardo Arroyo Ramírez <eduarroyo@MacBook-Air-de-Eduardo.local>
2019-03-07 22:52:20 +0100

    Encapsular consulta

--- bot.js
+++ bot.js
@@ -6,27 +6,33 @@ var bot;
 var MongoClient = require('mongodb').MongoClient;
 var connectionUrl = 'mongodb+srv://lector:TGRXOtj4KAdlxQI9@monitordb-9o1ct.mongodb.net/MonitorDb';
 
-try {
-    MongoClient.connect(connectionUrl, function(err1, client) {
-        if(err1) {
-            console.log("Error al obtener la conexión", err1);
-        }
-        console.log("Conexión al servidor de bases de datos.");
-        var db = client.db("MonitorDb");
-        console.log("Obtenida la base de datos");
-        var devices = db.collection("devices");
-        console.log("Obtenida la colección devices");
-
-        devices.find().toArray(function(err2, docs) {
-            if(err2) {
-                console.log("Error al realizar la consulta", JSON.stringify(err));
-            } else {
-                console.log("Consulta correcta", docs);
+function consultarEstados(exito, error) {
+    try {
+        MongoClient.connect(connectionUrl, function(err1, client) {
+            if(err1) {
+                console.log("Error al obtener la conexión", err1);
+                error(err1);
             }
+            console.log("Conexión al servidor de bases de datos.");
+            var db = client.db("MonitorDb");
+            console.log("Obtenida la base de datos");
+            var devices = db.collection("devices");
+            console.log("Obtenida la colección devices");
+
+            devices.find().toArray(function(err2, docs) {
+                if(err2) {
+                    console.log("Error al realizar la consulta", JSON.stringify(err));
+                    error(err2);
+                } else {
+                    exito(docs);
+                    console.log("Consulta correcta", docs);
+                }
+            });
         });
-    });
-} catch (error) {
-    console.log("Excepción al realizar consulta", JSON.stringify(error));
+    } catch (err2) {
+        console.log("Excepción al realizar consulta", JSON.stringify(error));
+        error(err3);
+    }
 }
 
 
